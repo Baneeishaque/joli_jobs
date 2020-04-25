82c501c697b5b894b6f546c4f1b46943b197418e
Banee Ishaque K <Baneeishaque@gmail.com>
2020-04-25 11:32:58 +0000

    db fix

--- admin/db.php
+++ admin/db.php
@@ -1,13 +1,15 @@
 <?php
 
+$cpanel_username="jolijobs";
+$cpanel_username_suffix="_";
+
 $server = "localhost";
-$server_user = "root";
-$server_pass = "lockit";
-$server_db = "j_jobs";
+$server_user = $cpanel_username.$cpanel_username_suffix."root";
+$server_pass = "aA9895204814";
+$server_db = $cpanel_username.$cpanel_username_suffix."db";
 
 $conn = new mysqli($server, $server_user, $server_pass, $server_db);
 if($conn->connect_error) {
+
 	die("connection failure" . $conn->connect_error);
 }
-
-?>
