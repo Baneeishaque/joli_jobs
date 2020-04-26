63945534996e2f31b6e06711fca2460502a12a4b
Banee Ishaque K <Baneeishaque@gmail.com>
2020-04-26 12:49:58 +0000

    db revert

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
 
-$cpanel_username="";
-$cpanel_username_suffix="";
+$cpanel_username="jolijobs";
+$cpanel_username_suffix="_";
 
 $server = "localhost";
 $server_user = $cpanel_username.$cpanel_username_suffix."root";
-$server_pass = "";
+$server_pass = "aA9895204814";
 $server_db = $cpanel_username.$cpanel_username_suffix."db";
 
 $conn = new mysqli($server, $server_user, $server_pass, $server_db);
