35f3258034357a1c77bf4183bd07425506a0b75a
Banee Ishaque K <Baneeishaque@gmail.com>
2020-04-26 16:03:32 +0000

    Ssquared.qa

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 
-$cpanel_username="jolijobs";
+$cpanel_username="ssquareq";
 $cpanel_username_suffix="_";
 
 $server = "localhost";
-$server_user = $cpanel_username.$cpanel_username_suffix."root";
+$server_user = $cpanel_username.$cpanel_username_suffix."dotlocus";
 $server_pass = "aA9895204814";
-$server_db = $cpanel_username.$cpanel_username_suffix."db";
+$server_db = $cpanel_username.$cpanel_username_suffix."jolijobs";
 
 $conn = new mysqli($server, $server_user, $server_pass, $server_db);
 if($conn->connect_error) {
