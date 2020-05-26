979db275a4575b52eb0e635e442aabb0a2a3508b
Banee Ishaque K <baneeishaque@gmail.com>
2020-05-27 00:58:38 +0530

    multi serve db

--- admin/db.php
+++ admin/db.php
@@ -1,15 +1,15 @@
 <?php
 
-$cpanel_username="";
-$cpanel_username_suffix="";
+$cpanel_username = "jolijobs";
+$cpanel_username_suffix = "_";
 
 $server = "localhost";
-$server_user = $cpanel_username.$cpanel_username_suffix."root";
-$server_pass = "";
-$server_db = $cpanel_username.$cpanel_username_suffix."jolijobs";
+$server_user = $cpanel_username . $cpanel_username_suffix . "root";
+$server_pass = "aA9895204814";
+$server_db = $cpanel_username . $cpanel_username_suffix . "db";
 
 $conn = new mysqli($server, $server_user, $server_pass, $server_db);
-if($conn->connect_error) {
+if ($conn->connect_error) {
 
     die("connection failure" . $conn->connect_error);
 }
